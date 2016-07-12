111ab5f5a61794e7a1c56dff27f14e0783fc58cc
Sergei Kissel <sergei.kissel@gmail.com>
2016-07-12 11:38:26 +0300

    JSandJQ

---
+++ JSandJQ.js
@@ -0,0 +1,24 @@
+/*
+var scores = [24, 32, 17, 44]
+var i = 0
+
+for (;scores[i];) {
+  console.log('Score ' + (i + 1) + ' is: ' + scores[i])
+  ++i
+}
+*/
+
+/*
+var scores = [24, 32, 17, 44]
+var i = 0
+var msg = ''
+
+for (;scores[i];) {
+  msg += ('Score ' + (i + 1) + ' is: ')
+  msg += scores[i] + '\n'
+  i++
+}
+
+console.log(msg)
+*/
+
