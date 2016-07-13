1cff7aec0421bab28d10507ef78f782f971db836
Sergei Kissel <sergei.kissel@gmail.com>
2016-07-13 19:44:45 +0300

    rock paper scissors test page

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,16 +1,17 @@
-//var userInput = prompt('Do you choose rock, paper or scissors?')
-var userInput = 'rock'
+// var userInput = 'rock'
 var compPick = Math.random()
+var userInput
 
 game()
 
 function game () {
+  userInput = prompt('Do you choose rock, paper or scissors?')
   if (_verifyUserInput() === userInput) {
-    console.log(_compChoice())
-    console.log(_compare(userInput, compPick))
+    document.write(_compChoice())
+    document.write(_compare(userInput, compPick))
   }
   else {
-    console.log('Try again. You can only choose rock, paper or scissors.')
+    document.write('Try again. You can only choose rock, paper or scissors.' + '</br>')
     game()
   }
 }
@@ -32,25 +33,25 @@ function _compChoice () {
   } else {
     compPick = 'scissors'
   }
-  return ('Computer: ' + compPick)
+  return ('Computer: ' + compPick + '</br>')
 }
 
 function _compare (choice1, choice2) {
-  if (choice1 === choice2) { return 'The result is a tie!' }
+  if (choice1 === choice2) { return 'The result is a tie!' + '</br>' }
 
   else if (choice1 === 'rock') {
-    if (choice2 === 'scissors') { return 'rock wins' }
-    else { return 'paper wins' }
+    if (choice2 === 'scissors') { return 'rock wins' + '</br>' }
+    else { return 'paper wins' + '</br>' }
   }
 
   else if (choice1 === 'paper') {
-    if (choice2 === 'rock') { return 'paper wins' }
-    else { return 'scissors wins' }
+    if (choice2 === 'rock') { return 'paper wins' + '</br>' }
+    else { return 'scissors wins' + '</br>' }
   }
 
   else if (choice1 === 'scissors') {
-    if (choice2 === 'rock') { return 'rock wins' }
-    else { return 'scissors wins' }
+    if (choice2 === 'rock') { return 'rock wins' + '</br>' }
+    else { return 'scissors wins' + '</br>' }
   }
 }
 
