078fd13aa9b8730922413f01a723ddd0055e7de2
sacredfire <sergei.kissel@gmail.com>
2016-07-19 23:33:57 +0300

    contactlist

---
+++ contactlist.js
@@ -0,0 +1,31 @@
+var friends = {}
+friends.bill = {}
+friends.steve = {}
+
+friends.bill.firstName = 'Bill'
+friends.bill.lastName = 'Fucker'
+friends.bill.number = '33839393'
+friends.bill.address = ['One Microsoft Way', 'Redmond', 'WA', '98052']
+
+friends.steve.firstName = 'Steve'
+friends.steve.lastName = 'Fu'
+friends.steve.number = '1111111'
+friends.steve.address = ['Fuckington', 'Redmond', 'WA', '98052']
+
+function list (obj) {
+  for (var key in obj) {
+    console.log(key)
+  }
+}
+
+function search (name) {
+  for (var key in friends) {
+    if (friends[key].firstName === name) {
+      console.log(friends[key])
+      return friends[key]
+    }
+  }
+}
+
+list(friends)
+search('Steve')
