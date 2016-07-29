2679a76f49fe17513b52992744964a976e29430d
sacredfire <sergei.kissel@gmail.com>
2016-07-29 13:01:31 +0300

    obj_constr

---
+++ obj_constr.js
@@ -0,0 +1,37 @@
+// function Person (name, age) {
+//   this.name = name
+//   this.age = age
+//   this.species = "Homo Sapiens"
+// }
+
+// var sally = new Person('Sally Bowles', 39)
+// var holden = new Person('Holden Caulfield', 16)
+// console.log("sally's species is " + sally.species + " and she is " + sally.age)
+// console.log("holden's species is " + holden.species + " and he is " + holden.age)
+
+// // Our Person constructor
+// function Person (name, age) {
+//   this.name = name
+//   this.age = age
+// }
+
+// // Now we can make an array of people
+// var family = []
+
+// family[0] = new Person('alice', 40)
+// family[1] = new Person('bob', 42)
+// family[2] = new Person('michelle', 8)
+// family[3] = new Person('timmy', 6)
+
+// // loop through our new array
+
+// for (var i = 0; family[i]; ++i) {
+//   console.log(family[i].name)
+// }
+
+var spencer2 = new Object()
+
+spencer2.age = 22
+spencer2.country = "United States"
+
+console.log(spencer2)
