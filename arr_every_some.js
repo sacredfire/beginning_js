87db61364c89a83c9cefec353cd096308269aa80
Sergei Kissel <sergei.kissel@gmail.com>
2016-10-02 14:18:39 +0300

    test

--- arr_every_some.js
+++ arr_every_some.js
@@ -6,3 +6,5 @@ a.every(function (x) { return x % 2 === 0 }) // => false: not all values even.
 var a1 = [1, 2, 3, 4, 5]
 a1.some(function (x) { return x % 2 === 0 }) // => true a has some even numbers.
 a1.some(isNaN) // => false: a has no non-numbers.
+
+
