4e507e9e5a3282640a59e26bd12f09c8dc41c071
sacredfire <sergei.kissel@gmail.com>
2017-12-01 13:29:19 +0300

    mutiply array

--- arr_1-100.js
+++ arr_1-100.js
@@ -1,4 +1,4 @@
-var foo = []
+/* var foo = []
 var foo1 = []
 // var sum = 0
 // var sum1 = 0
@@ -10,25 +10,25 @@ for (var i = 1; i <= 2001; i++) {
   }
 }
 
-var sum = foo.reduce((a, b) => a + b, 0)
+var sum = foo.reduce((a, b) => a + b, 0) */
 
 // for (var y = 0; y < foo.length; y++) {
 //   sum = sum + foo[y]
 // }
 
-for (var l = 1; l <= 2001; l++) {
+/* for (var l = 1; l <= 2001; l++) {
   if (l % 2 !== 0) {
     foo1.push(l)
   }
 }
 
-var sum1 = foo1.reduce((a, b) => a + b, 0)
+var sum1 = foo1.reduce((a, b) => a + b, 0) */
 
 // for (var d = 0; d < foo1.length; d++) {
 //   sum1 = sum1 + foo1[d]
 // }
 
-console.log('even arr sum =', (sum))
+/* console.log('even arr sum =', (sum))
 console.log('odd arr sum =', (sum1))
 
 delta = Math.abs(sum - sum1)
@@ -37,4 +37,23 @@ if (sum < sum1) {
   console.log('odd arr sum is bigger than even arr sum by', (delta))
 } else {
   console.log('even arr sum is bigger than odd arr sum by', (delta))
+} */
+
+var arr12_40 =[]
+
+for (var h = 12; h < 41; h++) {
+  arr12_40.push(h)
 }
+
+console.log(arr12_40)
+console.log(arr12_40[11])
+
+function multiply (array) {
+  var sum = 1
+  for (var i = 0; i < array.length; i++) {
+    sum = sum * array[i];
+  }
+  return sum;
+}
+
+console.log(multiply(arr12_40))
